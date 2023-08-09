cc36e8f02fe00d338cb24b219a759331fe94a47f
shane04111 <shane97213@gmail.com>
2023-08-10 01:18:37 +0800

    Initial commit

---
+++ src/main/java/name/nospider/NospiderClient.java
@@ -0,0 +1,10 @@
+package name.nospider;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class NospiderClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/name/nospider/NospiderDataGenerator.java
@@ -0,0 +1,11 @@
+package name.nospider;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class NospiderDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
