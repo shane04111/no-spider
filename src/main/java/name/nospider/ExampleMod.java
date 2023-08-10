4fe6e53d26554af4a2be405f5cec516c3d33624c
shane04111 <shane97213@gmail.com>
2023-08-10 19:32:33 +0800

    1.19.4

---
+++ src/main/java/name/nospider/ExampleMod.java
@@ -0,0 +1,21 @@
+package name.nospider;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerEntityEvents;
+import net.minecraft.entity.mob.CaveSpiderEntity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.mob.SpiderEntity;
+public class ExampleMod implements ModInitializer {
+
+	@Override
+	public void onInitialize() {
+		ServerEntityEvents.ENTITY_LOAD.register((entity, world) -> {
+			if (entity.getType() == EntityType.SPIDER && entity instanceof SpiderEntity) {
+				// 取消蜘蛛的生成
+				entity.discard();
+			} else if (entity.getType() == EntityType.CAVE_SPIDER && entity instanceof CaveSpiderEntity) {
+				entity.discard();
+			}
+		});
+	}
+}
