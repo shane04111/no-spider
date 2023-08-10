408beeb4861f20f565d9a66cdf0da3450a3a3138
shane04111 <shane97213@gmail.com>
2023-08-10 14:49:42 +0800

    刪除蜘蛛與洞穴蜘蛛生成

--- src/main/java/name/nospider/Nospider.java
+++ src/main/java/name/nospider/Nospider.java
@@ -2,22 +2,29 @@ package name.nospider;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerEntityEvents;
+import net.minecraft.entity.mob.CaveSpiderEntity;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.mob.SpiderEntity;
 
 public class Nospider implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final String MOD_ID = "no-spider";
     public static final Logger LOGGER = LoggerFactory.getLogger("no-spider");
 
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
+		// 注册实体加载事件，当实体被加载时触发
+		ServerEntityEvents.ENTITY_LOAD.register((entity, world) -> {
+			if (entity.getType() == EntityType.SPIDER && entity instanceof SpiderEntity) {
+				// 取消蜘蛛的生成
+				entity.discard();
+			} else if (entity.getType() == EntityType.CAVE_SPIDER && entity instanceof CaveSpiderEntity) {
+				entity.discard();
+			}
+		});
+
 	}
 }
